7a9289bf5db70c0e9c7537689c7984ae782ef3e8
SARAVANAKANTH <93504039+SARAVANAKANTH@users.noreply.github.com>
2022-10-16 13:58:50 +0530

    Add files via upload

---
+++ project/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import SwipeDrawer from './SwipeDrawer';
+import Notes from './notes/Notes';
+import Archives from './archives/Archives';
+import DeleteNotes from './delete/DeleteNotes';
+
+const Home = () => {    
+    return (
+        <Box style={{ display: 'flex', width: '100%' }}>
+            <Router>
+                <SwipeDrawer />
+                <Routes>        
+                    <Route path='/' element={<Notes />} />
+                    <Route path='/archive' element={<Archives />} />
+                    <Route path='/delete' element={<DeleteNotes />} />
+                </Routes>
+            </Router>
+        </Box>
+    )
+}
+
+export default Home;
