13facd990525909ae38bef821b7bae9427f35c38
SARAVANAKANTH <93504039+SARAVANAKANTH@users.noreply.github.com>
2022-10-16 14:18:48 +0530

    Add files via upload

---
+++ build/precache-manifest.9340966696681219261255976e5108a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72955bc32cb4e37ce468",
+    "url": "/static/css/main.b5e3337d.chunk.css"
+  },
+  {
+    "revision": "72955bc32cb4e37ce468",
+    "url": "/static/js/main.6380ec62.chunk.js"
+  },
+  {
+    "revision": "b0f06cba259300652720",
+    "url": "/static/js/runtime~main.a178f2df.js"
+  },
+  {
+    "revision": "8a59312a3500f9df1da8",
+    "url": "/static/js/2.6607768f.chunk.js"
+  },
+  {
+    "revision": "bd5486bf8e1a315864fd",
+    "url": "/static/js/3.bf3d30a9.chunk.js"
+  },
+  {
+    "revision": "a908debcc7b26593b5527ce88edd8039",
+    "url": "/index.html"
+  }
+];
